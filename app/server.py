d3c9dde81fe5eab09abb81cfb658aab4d4288f02
Pythos Labs <48703756+pythoslabs@users.noreply.github.com>
2019-08-01 23:19:13 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/tugit1fd7kf449o/learner_vehicle_res50.pkl?dl=1'
+export_file_name = 'learner_vehicle_res50.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bus', 'car', 'lorry', 'tanker', 'truck']
 path = Path(__file__).parent
 
 app = Starlette()
